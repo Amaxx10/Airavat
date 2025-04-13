7ab707634e120612ca4708dc543fece7f8600980
Amaxx10 <aman.kanojia22@spit.ac.in>
2025-04-13 06:02:48 +0530

    1234

--- server/routes/query_routes.js
+++ server/routes/query_routes.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { get_query, upload } from '../controllers/query_controller.js';
 const router = express.Router();
 
-router.post('/query', upload.single('image'), get_query);
+router.post('/get_query', upload.single('image'), get_query);
 
 export default router;
